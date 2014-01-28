986c7a716137e999bd3db4433e3eb362be50280a
Christian <chrisocon60@gmail.com>
2014-01-27 20:58:47 -0500

    changed port of listener

--- test2/server.js
+++ test2/server.js
@@ -16,7 +16,7 @@ function start(route) {
     response.end();
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(5000);
   console.log("Server has started.");
 }
 
